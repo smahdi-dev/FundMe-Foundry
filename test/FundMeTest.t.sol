7719f4ef22fdc9e6c6b226e5b01c89c76fc11e37
smahdi-dev <seyedmahdi.hosseini24@gmail.com>
2024-10-21 21:49:47 +0330

    gas calculations removed. it was just a practice.

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -11,7 +11,6 @@ contract FundMeTest is Test {
 
     uint256 constant SEND_VALUE = 0.1 ether;
     uint256 constant STARTING_BALANCE = 10 ether;
-    uint256 constant GAS_PRICE = 1;
     address USER = makeAddr("user");
 
     function setUp() external {
@@ -91,17 +90,9 @@ contract FundMeTest is Test {
         uint256 startingOwnerBalance = fundMe.getOwner().balance;
 
         // Act
-        uint256 gasStart = gasleft();
-
-        vm.txGasPrice(GAS_PRICE);
         vm.startPrank(fundMe.getOwner());
         fundMe.withdraw();
         vm.stopPrank();
-
-        uint256 gasEnd = gasleft();
-
-        uint256 gasUsed = (gasStart - gasEnd) * tx.gasprice;
-        console.log(gasUsed);
 
         // Assert
         assert(address(fundMe).balance == 0);
